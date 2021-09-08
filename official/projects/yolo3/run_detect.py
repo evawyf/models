29ddfaad04a98467dd72758cdb0dfdb10d62c514
evawyf <evawyf1@gmail.com>
2021-09-07 21:26:18 -0700

    config/task/model

--- official/projects/yolo3/run_detect.py
+++ official/projects/yolo3/run_detect.py
@@ -7,8 +7,8 @@ from absl import app
 # pylint: disable=unused-import
 from official.common import flags as tfm_flags
 from official.projects.basnet.configs import yolo3 as yolo3_cfg
-# from official.projects.basnet.modeling import yolo3_model
-# from official.projects.basnet.tasks import basnet as basenet_task
+from official.projects.basnet.modeling import yolo3_model
+from official.projects.basnet.tasks import yolo3 as yolo3_task
 from official.vision.beta import train
 
 
